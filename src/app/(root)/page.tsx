8e350ac9caa9708ee0bb6052005937455b7eb0f7
german <germanalvarez14@gmail.com>
2023-09-16 20:25:12 -0700

    make dropdown list work

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,10 @@
+import { Navbar } from "@/components/navbar";
 import Image from "next/image";
 
 export default function Home() {
-  return <p>hi</p>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,22 @@
+"use client";
+import Link from "next/link";
+import { MainNav } from "./main-nav";
+
 export interface NavbarProps {}
 
 export const Navbar = (props: NavbarProps) => {
   return (
     <>
-      <h1>Shop</h1>
+      <header>
+        <nav
+          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div className="flex lg:flex-1">
+            <MainNav />
+          </div>
+        </nav>
+      </header>
     </>
   );
 };
