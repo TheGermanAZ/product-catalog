3cb8af2406e142d9551a9ede0d1c2b359d891b62
german <germanalvarez14@gmail.com>
2025-01-11 22:32:22 -0800

    update packages and is broken

--- src/app/clubs/[product]/page.tsx
+++ src/app/clubs/[product]/page.tsx
@@ -5,8 +5,9 @@ type Props = {};
 
 const club = ({ params }: { params: { product: string } }): JSX.Element => {
   const _product = params.product.split("-").join(" ");
-  const id = params.product.split("_").at(-1) as string;
-  const product = _product.split("_").at(0) as string;
+  const [product, id] = _product.split("_").join(" ");
+  const id1 = params.product.split("_").at(-1) as string;
+  const product1 = _product.split("_").at(0) as string;
   return <ProductInfo product={product} id={id} />;
 };
 
